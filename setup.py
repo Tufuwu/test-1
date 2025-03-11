0abedf37a9b8115a4a83d7a8c2755102696955b9
wu <2450402591@qq.com>
2025-03-11 20:20:44 +0800

    molmed/checkqc0

--- setup.py
+++ setup.py
@@ -1,72 +1,31 @@
 from setuptools import setup, find_packages
-from os import path
+from checkQC import __version__
 
-
-here = path.abspath(path.dirname(__file__))
-about = {}
-with open(path.join(here, 'vakt', 'version.py'), mode='r', encoding='utf-8') as f:
-    exec(f.read(), about)
-
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
-
-if __name__ == '__main__':
-    setup(
-        name='vakt',
-        description='Attribute-based access control (ABAC) SDK for Python',
-        keywords='ACL ABAC access-control policy security authorization permission',
-        version=about['__version__'],
-        author='Egor Kolotaev',
-        author_email='ekolotaev@gmail.com',
-        license="Apache 2.0 license",
-        url='http://github.com/kolotaev/vakt',
-        long_description=long_description,
-        long_description_content_type='text/markdown',
-        py_modules=['vakt'],
-        python_requires='>=3.6',
-        install_requires=[
-            'jsonpickle>=2.0',
-        ],
-        extras_require={
-            'dev': [
-                'pytest~=7.0',
-                'pytest-cov~=4.0',
-                'pylint~=2.13',
-                'PyMySQL~=1.0',
-                'psycopg2cffi~=2.8',
-            ],
-            'mongo': [
-                'pymongo~=4.1',
-            ],
-            'sql': [
-                'SQLAlchemy~=1.4',
-            ],
-            'redis': [
-                'redis~=4.3'
-            ],
-        },
-        packages=find_packages(exclude='tests'),
-        classifiers=[
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: Apache Software License',
-            'Operating System :: OS Independent',
-            'Topic :: System :: Systems Administration',
-            'Topic :: System :: Networking',
-            'Topic :: System :: Networking :: Firewalls',
-            'Topic :: Security',
-            'Topic :: Software Development',
-            'Topic :: Utilities',
-            'Natural Language :: English',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
-            'Programming Language :: Python :: 3.10',
-            'Programming Language :: Python :: 3.11',
-            'Programming Language :: Python :: Implementation :: CPython',
-            'Programming Language :: Python :: Implementation :: PyPy',
-        ],
-    )
+setup(
+    name='checkQC',
+    version=__version__,
+    description="A simple program to parse Illumina NGS data and check it for quality criteria.",
+    long_description="A simple program to parse Illumina NGS data and check it for quality criteria.",
+    keywords=['bioinformatics', 'ngs', 'quality control'],
+    author='Johan Dahlberg, SNP&SEQ Technology Platform, Uppsala University',
+    author_email='johan.dahlberg@medsci.uu.se',
+    url="https://www.github.com/Molmed/checkQC",
+    download_url='https://github.com/Molmed/checkQC/archive/{}.tar.gz'.format(__version__),
+    python_requires='>3.10, <3.11',
+    install_requires=[
+        "click",
+        "PyYAML>=6.0",
+        "interop>=1.2.4",
+        "xmltodict",
+        "tornado",
+        "sample_sheet"],
+    packages=find_packages(exclude=["tests*"]),
+    test_suite="tests",
+    package_data={'checkQC': ['default_config/config.yaml', 'default_config/logger.yaml']},
+    include_package_data=True,
+    license='GPLv3',
+    entry_points={
+        'console_scripts': ['checkqc = checkQC.app:start',
+                            'checkqc-ws = checkQC.web_app:start']
+    },
+)
